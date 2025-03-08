a40df5fb9eb14e5ae3a1d40e9a3082725f270fb8
youyu <129youyu@gmail.com>
2025-03-08 14:11:57 +0800

    修改api edit的錯誤

--- api/edit.php
+++ api/edit.php
@@ -6,7 +6,7 @@ if(isset($_POST['id'])){
         if(isset($_POST['del']) && in_array($id,$_POST['del'])){
             $Title->del($id);
         }else{
-            $row->$Title->find($id);
+            $row=$Title->find($id);
             $row['text']=$_POST['text'][$idx];
             $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
             $Title->save($row);
