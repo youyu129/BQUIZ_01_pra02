c030f57d30db1a11fe4ee1b690ebe5399c74a074
youyu129 <129youyu@gmail.com>
2025-03-10 01:55:52 +0800

    完成將進站總人數及頁尾版權顯示於畫面

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ include_once "api/db.php";
         <div style="clear:both;"></div>
         <div
             style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-            <span class="t" style="line-height:123px;"></span>
+            <span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
         </div>
     </div>
 
