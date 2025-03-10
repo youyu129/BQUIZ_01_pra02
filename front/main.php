b9b8f84f4fc60b89f419929ededb1eaea6aa6105
youyu129 <129youyu@gmail.com>
2025-03-10 15:45:08 +0800

    將動畫圖片顯示於首頁

--- front/main.php
+++ front/main.php
@@ -4,28 +4,13 @@
 <?php
 $rows=$Ad->all(['sh'=>1]);
 foreach($rows as $row){
-    echo $row['text'] . "&nbsp;&nbsp;&nbsp;&nbsp;";
+    echo $row['text'] . "&nbsp;&nbsp;&nbsp;";
 }
 ?>
 </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
-    <script>
-    var lin = new Array();
-    var now = 0;
-    if (lin.length > 1) {
-        setInterval("ww()", 3000);
-        now = 1;
-    }
-
-    function ww() {
-        $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
-        //$("#mwww").attr("src",lin[now])
-        now++;
-        if (now >= lin.length)
-            now = 0;
-    }
-    </script>
+    
     <div style="width:100%; padding:2px; height:290px;">
         <div id="mwww" loop="true" style="width:100%; height:100%;">
             <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
@@ -53,5 +38,29 @@ foreach($rows as $row){
             }
         )
         </script>
+        <script>
+
+    var lin = new Array();
+    <?php
+    $rows=$Mvim->all(['sh'=>1]);
+    foreach($rows as $row){
+        echo "lin.push('upload/{$row['img']}');";
+    }
+    ?>
+    var now = 0;
+    if (lin.length > 1) {
+        setInterval("ww()", 3000);
+        // now = 1;
+    }
+
+    function ww() {
+        $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+        //$("#mwww").attr("src",lin[now])
+        now++;
+        if (now >= lin.length)
+            now = 0;
+    }
+    ww();
+    </script>
     </div>
 </div>
